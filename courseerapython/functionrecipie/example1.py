431c0a0db3b7ef4b7bf7ebf5897e9c540f1e0ee6
charan2108 <sumacharan.adabala@gmail.com>
2022-06-11 12:49:48 +0530

    added new folder

---
+++ courseerapython/functionrecipie/example1.py
@@ -0,0 +1,5 @@
+def convert_to_celsius(fahrenheit):
+    return fahrenheit - 32 * 5/ 9
+
+temperature = convert_to_celsius(40)
+print(temperature)
---
+++ courseerapython/test1.py
@@ -0,0 +1,23 @@
+# value = 8.564
+# result =round(value)
+# print(result)
+
+def bigger(x):
+    return x ** x
+bigger(12)
+
+
+# print(round(45.345, 2))
+# print(round(45.8))
+# print(round(45))
+# print(round(45.345, 2, 5))
+
+
+earthquake_greeting = "I'm feeling a bit shaky"
+print(earthquake_greeting)
+earthquake_greeting = 'I’m feeling a bit shaky'
+print(earthquake_greeting)
+earthquake_greeting = "I\’m feeling a bit shaky"
+print(earthquake_greeting)
+earthquake_greeting = 'I\’m feeling a bit shaky'
+print(earthquake_greeting)
